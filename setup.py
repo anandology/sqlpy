ae03a4059554497fe4180d5134ffd9d5b1db06cb
Anand Chitipothu <anandology@gmail.com>
2013-01-19 22:29:59 +0530

    skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="sqlpy",
+    version="0.1-dev",
+    description="The database module of web.py",
+    packages=["sqlpy"],
+    platforms=["any"],
+)
